7fe9547c8d7859124f7d462db81a747b9a9fdb84
MadsRF <madsrunefrederiksen@gmail.com>
2020-04-30 11:52:06 +0200

    comments on code and a bit of clean up

--- public/frontpage/frontpage.js
+++ public/frontpage/frontpage.js
@@ -4,21 +4,29 @@ console.log("frontpage.js works");
 //waits for the html doc to be ready before atempting to run any js.
 $(document).ready( () =>{
 
-    // jquery getting our json order data from firebase
+    // jquery getting our json order data from API
     $.get("http://localhost:8888/orderslist", (data) => {    
        
+        // loops through our orderlist api
         let rows = data.map(item => {
           let $clone = $('#frontpage_new_ordertable tfoot tr').clone();
-          $clone.find('.customer-name').text(item.customer_name);
+          $clone.find('.customer_name').text(item.customer_name);
           $clone.find('.date').text(item.date);
           $clone.find('.time').text(item.time);
           $clone.find('.total').text(item.total + ' Kr.');
       
-          let products = item.products.map(prod => `${prod.name}: ${prod.price} Kr.`);
-          $clone.find('.products').html(products.join('<br />'));
+          // loops through orders product name
+          let productsName = item.products.map(prod => `${prod.name}`);
+          $clone.find('.products').html(productsName.join('<br />'));
+          
+          // loops through orders product price
+          let productsPrice = item.products.map(prod => `${prod.price} Kr.`);
+          $clone.find('.price').html(productsPrice.join('<br />'));
+          
+          
           return $clone;
         });
-      
+        //appends to our frontpage html 
         $("#frontpage_new_ordertable tbody").append(rows);
     });
 
