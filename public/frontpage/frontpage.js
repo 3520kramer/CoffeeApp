3a4098690c17d466fdcc94cb11bd7b76247cf644
MadsRF <madsrunefrederiksen@gmail.com>
2020-05-14 09:03:00 +0200

    safty push

--- public/frontpage/frontpage.js
+++ public/frontpage/frontpage.js
@@ -4,6 +4,20 @@ console.log("frontpage.js works");
 //waits for the html doc to be ready before atempting to run any js.
 $(document).ready( () =>{
 
+    // add event listener here
+    $(document).on("click", ".acceptOrder", foo);
+
+    // add the event handler at the top-level inside doc.ready
+    function foo() {
+      var btn = $(this);
+      var row = btn.closest("tr");
+      var id = row.data("id");
+      var name = row.find(".customer_name").text();
+      console.log(id, name)
+    }
+
+
+
     // jquery getting our json order data from API
     $.get("http://localhost:8888/orderslist", (data) => {    
        
@@ -11,6 +25,7 @@ $(document).ready( () =>{
         // loops through our orderlist api
         let rows = data.map(item => {
           let $clone = $('#frontpage_new_ordertable tfoot tr').clone();
+          $clone.data("id", item.id);
           $clone.find('.customer_name').text(item.customer_name);
           $clone.find('.date').text(item.date);
           $clone.find('.time').text(item.time);
@@ -18,15 +33,13 @@ $(document).ready( () =>{
           $clone.find('.comments').text(item.comments);
           $clone.find('.total').text(item.total + ' Kr.');
 
-          let foo = function(){
-            console.log(item.id)
-          }
 
           // accept and cancel buttons
           $clone.find('.order_status').html(
-            `<button type="button" onclick="${foo()}">Accept</button>` + 
-            `<button type="button" onclick="alert(${item.total})">Cancel</button>`
+            `<button class="acceptOrder" type="button">Accept</button>` + 
+            `<button class="cancelOrder" type="button">Cancel</button>`
           );
+
           // loops through orders product name
           let productsName = item.products.map(prod => `${prod.name}`);
           $clone.find('.products').html(productsName.join('<br />'));
@@ -35,14 +48,10 @@ $(document).ready( () =>{
           let productsPrice = item.products.map(prod => `${prod.price} Kr.`);
           $clone.find('.price').html(productsPrice.join('<br />'));
           
-    
-          
           return $clone;
         });
 
-       
-
-
+      
 
         //appends to our frontpage html 
         $("#frontpage_new_ordertable tbody").append(rows);
